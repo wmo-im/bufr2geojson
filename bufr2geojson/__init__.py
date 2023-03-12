876c6bc19231b5dce773e260e41787240bb713db
Tom Kralidis <tomkralidis@gmail.com>
2023-03-12 18:25:45 -0400

    update release version v0.5.0

--- bufr2geojson/__init__.py
+++ bufr2geojson/__init__.py
@@ -19,7 +19,7 @@
 #
 ###############################################################################
 
-__version__ = "0.5.dev0"
+__version__ = "0.5.0"
 
 from collections import OrderedDict
 from copy import deepcopy
