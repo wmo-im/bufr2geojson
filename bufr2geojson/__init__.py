c4f6173110b975d73b04c939d34f06bd634c21cd
david-i-berry <dberry@wmo.int>
2023-05-14 14:57:39 +0200

    Fix for when numeric passed to strip2 as part of wsi / ID parsing. (#23)
    * Fix for when numeric passed to strip2 as part of wsi / ID parsing.
    
    * Fix for when numeric passed to strip2 as part of wsi / ID parsing.

--- bufr2geojson/__init__.py
+++ bufr2geojson/__init__.py
@@ -880,12 +880,16 @@ def strip2(value) -> str:
     :returns: `str` of stripped value
     """
 
-    space = None
+    if value is None:
+        return None
 
     if isinstance(value, str):
         space = ' '
     elif isinstance(value, bytes):
         space = b' '
+    else:  # make sure we have a string
+        space = ' '
+        value = f"{value}"
 
     if value.startswith(space) or value.endswith(space):
         LOGGER.warning(f"value '{value}' is space padded; upstream data should be fixed")  # noqa
