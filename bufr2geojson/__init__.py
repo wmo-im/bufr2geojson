dcf9bbf1aca6b287179d31805727bb28da609f2d
david-i-berry <dberry@wmo.int>
2023-09-28 16:44:30 +0200

    back to dev.

--- bufr2geojson/__init__.py
+++ bufr2geojson/__init__.py
@@ -19,7 +19,7 @@
 #
 ###############################################################################
 
-__version__ = "0.5.1"
+__version__ = "0.5.dev2"
 
 from collections import OrderedDict
 from copy import deepcopy
