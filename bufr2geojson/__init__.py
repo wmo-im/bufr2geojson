3be7addb191430f61000035d67360af5e4932836
david-i-berry <dberry@wmo.int>
2023-09-28 16:40:28 +0200

    Bump version #.

--- bufr2geojson/__init__.py
+++ bufr2geojson/__init__.py
@@ -19,7 +19,7 @@
 #
 ###############################################################################
 
-__version__ = "0.5.0"
+__version__ = "0.5.1"
 
 from collections import OrderedDict
 from copy import deepcopy
